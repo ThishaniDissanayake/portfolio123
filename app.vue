38f0a2bad987bf0f6013798bcebe3efe33799cf0
Thishani Dissanayake <dissanayakenisansala2000@gmail.com>
2024-05-24 12:13:34 +0530

    first commit

--- app.vue
+++ app.vue
@@ -1,5 +1,44 @@
 <template>
   <div>
-    <p>hello world</p>
+    <h1>{{ name }}</h1>
+
+    <div v-if="pending">Loading projects...</div>
+    <div v-if="pendingBlogs">Loading blogs...</div>
+
+    <h1>Projects</h1>
+
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
+      </li>
+    </ul>
+  </div>
+  <div>
+    <h1>Blogs</h1>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.title">
+        <h2>{{ blog.title }}</h2>
+        <p>{{ blog.content }}</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+
+
+const name = "Thishani";
+const {
+  data: projects,
+  pending: pendingProjects,
+  error: errorProjects,
+} = useFetch("http://localhost:5000/projects");
+
+const {
+  data: blogs,
+  pending: pendingBlogs,
+  error: errorBlogs,
+} = useFetch("http://localhost:5000/blogs");
+
+</script>
